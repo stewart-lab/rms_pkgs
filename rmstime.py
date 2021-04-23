71454e5a1fa780aea51875b5aeb4c7fc2560fca4
Ron Stewart <rstewart@morgridge.org>
2021-04-23 12:26:47 -0500

    changed get_time to get_time_and_pretty_time

--- rmstime.py
+++ rmstime.py
@@ -2,7 +2,7 @@
 import time
 
 # ---------------  FUNCTIONS -------------------
-def get_time():
+def get_time_and_pretty_time():
     time_struct = time.localtime()
     time_secs = time.mktime(time_struct)  #need to use something besides time.mktime if want fractions of sec.
     pretty_time = time.strftime("%d_%b_%Y_%H_%M_%S", time_struct)
