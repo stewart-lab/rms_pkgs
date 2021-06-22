2b05db9bad2ccc2b7b49e7ef161ca8cfa398bfe1
Ron Stewart <rstewart@morgridge.org>
2021-06-22 15:55:30 -0500

    changed pretty_start_time to YYYY_MM_DD_HH_MM_SS format

--- rmstime.py
+++ rmstime.py
@@ -5,7 +5,8 @@ import time
 def get_time_and_pretty_time():
     time_struct = time.localtime()
     time_secs = time.mktime(time_struct)  #need to use something besides time.mktime if want fractions of sec.
-    pretty_time = time.strftime("%d_%b_%Y_%H_%M_%S", time_struct)
+    #pretty_time = time.strftime("%d_%b_%Y_%H_%M_%S", time_struct)  #old format
+    pretty_time = time.strftime("%Y_%m_%d_%H_%M_%S", time_struct)  #New format that will sort better
     return(time_secs, pretty_time)
     
     
