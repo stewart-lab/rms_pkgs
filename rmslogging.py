8947f0703ddba584481517caaad64275b1f98e01
Ron Stewart <rstewart@morgridge.org>
2021-08-13 15:54:36 -0500

    write out executing file and command line file

--- rmslogging.py
+++ rmslogging.py
@@ -13,8 +13,17 @@ def open_log_file(log_file):
 def close_log_file(log_file_fh):
     log_file_fh.close()
         
-def write_args(my_args, logfile):
+def write_args_and_files (my_args, curr_executing_file, command_line_file, logfile):
     logfile.write("args:\n")
     for attr, value in my_args.items():
-        logfile.write(str(attr) + ":" + str(value) + "\n")       
+        logfile.write(str(attr) + ":" + str(value) + "\n") 
+    write_file_contents(curr_executing_file, logfile)
+    write_file_contents(command_line_file, logfile)
+        
+def write_file_contents(a_file, logfile):
+    logfile.write("\n\n-------------- START " + a_file + " -----------------\n")
+    with open(a_file, "r") as file_to_write_out:
+        for line in file_to_write_out:
+            logfile.write(str(line))
+    logfile.write("\n\n-------------- END " + a_file + " -----------------\n")              
     
